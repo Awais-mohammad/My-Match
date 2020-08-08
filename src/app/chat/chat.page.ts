7aac6c190a03af94b479e818ff1058a84be5c7bb
Awais-mohammad <muhammadawais083@gmail.com>
2020-08-08 11:37:32 +0500

    realtimechar

--- src/app/chat/chat.page.ts
+++ src/app/chat/chat.page.ts
@@ -1,3 +1,4 @@
+import { FirebaseAuth } from "angularfire2";
 import { Message } from "@angular/compiler/src/i18n/i18n_ast";
 import { AngularFirestore } from "angularfire2/firestore";
 import { AngularFireAuth } from "angularfire2/auth";
@@ -5,50 +6,76 @@ import { firestore } from "firebase/app";
 import { Observable } from "rxjs";
 import { AuthService } from "./../auth.service";
 
-import {
-  Component,
-  OnInit,
-  Input,
-  ChangeDetectionStrategy,
-} from "@angular/core";
+import { Component, OnInit, Input, ChangeDetectionStrategy } from "@angular/core";
 import { ModalController } from "@ionic/angular";
 import { ActivatedRoute } from "@angular/router";
 import * as firebase from "firebase";
+import { Console } from "console";
+import { variable } from "@angular/compiler/src/output/output_ast";
 @Component({
   selector: "app-chat",
   templateUrl: "./chat.page.html",
   styleUrls: ["./chat.page.scss"],
 })
 export class ChatPage implements OnInit {
-  constructor(
-    private ModalController: ModalController,
-    private route: ActivatedRoute,
-    public auth: AuthService,
-    private firestore: AngularFirestore,
-    private firebaseAuth: AngularFireAuth
-  ) {}
+  constructor(private ModalController: ModalController, private route: ActivatedRoute, public auth: AuthService, private firestore: AngularFirestore, private firebaseAuth: AngularFireAuth) {
+    this.checkBothUsersPreviousChat();
+  }
 
   @Input() userData;
   msg: any[];
   closeModal() {
     this.ModalController.dismiss();
   }
+  chat: any[];
+  checkBothUsersPreviousChat() {
+    this.firestore
+      .collection("chats")
+      .valueChanges()
+      .subscribe((res) => {
+        this.chat = res;
+        for (var i = 0; i > this.chat.length; i++) {
+          if (this.chat[i].userID == this.Id && this.chat[i].secondUser == this.userData.userID) {
+            console.log("Users previous chat found");
+          } else {
+            console.log("Users previous chat not found");
+          }
+        }
+      });
+  }
 
   sendmsg() {
-  
-   
+    const userID = this.Id;
+    const secondUser = this.userData.userID;
+    const Chat = this.msg;
+    this.firestore
+      .collection("chats")
+      .add({
+        secondUser,
+        userID,
+        messages: firebase.firestore.FieldValue.arrayUnion({
+          Chat,
+        }),
+      })
+      .then((ref) => {
+        console.log(ref.id);
+        const docID = ref.id;
+        this.firestore.collection("chats").doc(ref.id).update({
+          docID,
+        });
+      });
+    console.log("msg will be sent");
   }
   Id: string;
   meesagedata: any;
 
-  getmessages() {
-   
-  }
-/*chk */
+  getmessages() {}
+
   ngOnInit() {
     this.getmessages();
-    const userSub=this.firebaseAuth.auth.currentUser.uid;
-    this.Id=userSub;
-    console.log("user data->", this.userData.userID);
+    const userSub = this.firebaseAuth.auth.currentUser.uid;
+    this.Id = userSub;
+
+    this.checkBothUsersPreviousChat();
   }
 }
